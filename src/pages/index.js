8a49793dd7d9cebb90588fe86be5174704a10fa4
rdarida <robertdarida86@gmail.com>
2021-02-15 19:12:18 +0100

    feat: update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,20 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+export default function IndexPage() {
+  const email = 'hello@kreaktivlab.com';
 
-export default IndexPage
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Coming Soon!</h1>
+      <p>Something really good is coming very soon.</p>
+      <p>
+        If you have any question, please contact us at<br />
+        <a href={`mailto:${email}`}>{email}</a>
+      </p>
+    </Layout>
+  );
+}
