3f2287bc73cf68ecb53cd28d7baa2db34ba62f51
rdarida <robertdarida86@gmail.com>
2021-02-16 10:48:30 +0100

    feat: update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,13 +3,38 @@ import { Link } from 'gatsby';
 
 import { PageLayout, SEO } from '../components';
 
+const styles = {
+  pnf: {
+    padding: '100px 0 60px 0',
+    width: '100%',
+    margin: '0',
+    lineHeight: '1em',
+    textTransform: 'uppercase'
+  },
+  h1: {
+    fontSize: '16vmin'
+  }
+};
+
 export default function NotFoundPage(): JSX.Element {
   return (
     <PageLayout>
-      <SEO title="404: Not found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      <Link to="/">Back to Home</Link>
+      <SEO title="Page Not Found!" />
+
+      <div className="container-fluid text-center" style={styles.pnf}>
+        <div className="row">
+          <div className="col-12">
+            <h2 style={styles.h1}>Page<br />Not<br />Found!</h2>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <Link to="/" className="btn btn-primary btn-lg">Back to Home</Link>
+          </div>
+        </div>
+      </div>
+
     </PageLayout>
   );
 }
