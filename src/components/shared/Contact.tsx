c990c3073f6b1beaa5be55948e932dc5fd7063c8
rdarida <robertdarida86@gmail.com>
2021-02-16 09:10:17 +0100

    feat: implement Contact

--- src/components/shared/Contact.tsx
+++ src/components/shared/Contact.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+const data = {
+  email: 'hello@kreaktivlab.com',
+  faIcons: [
+    {
+      link: 'https://facebook.com/kreaktivlab',
+      color: '#3b5999',
+      shape: 'fa-circle',
+      icon: 'fa-facebook'
+    }
+  ]
+};
+
 export function Contact(): JSX.Element {
-  const email = 'hello@kreaktivlab.com';
+  const { email, faIcons } = data;
 
   return (
     <div id="Contact" className="container-fluid">
@@ -13,6 +25,15 @@ export function Contact(): JSX.Element {
           please contact us at <a href={`mailto:${email}`}>{email}</a>
         </div>
       </div>
+
+      <p className="social">
+        <a href="https://facebook.com/kreaktivlab" target="_blank">
+          <span className="fa-stack fa-lg" style={{color: '#3b5999'}}>
+            <i className={`fa fa-circle fa-stack-2x`} />
+            <i className={`fa fa-facebook fa-stack-1x fa-inverse`} />
+          </span>
+        </a>
+      </p>
     </div>
   );
 }
